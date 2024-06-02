8ae4d24e27edab66f04190e626405db094f87e98
ukrWily <ukrwily@gmail.com>
2024-06-02 13:10:07 +0300

    L getResource

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,9 +1,17 @@
-async function getResource(url) {
-  let res = await fetch(url);
+class MarvelService {
+  getResource = async (url) => {
+    let res = await fetch(url);
 
-  if (!res.ok) {
-    throw new Error(`Could not fetch ${url}, status: ${res.status}`);
-  }
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+    }
 
-  return await res.json();
+    return await res.json();
+  };
+
+  getAllCharacters = () => {
+    this.getResource = () => {
+      return this.getResource();
+    };
+  };
 }
