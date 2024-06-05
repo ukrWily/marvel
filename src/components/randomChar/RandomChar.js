8affe0c87ce851b86d25a249c42551ba125bd64f
”ukrWily” <ukrwily@gmail.com>
2024-06-05 20:11:41 +0300

    PC randomChar

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -30,37 +30,53 @@ class RandomChar extends Component {
       char: { name, description, thumbnail, homepage, wiki },
     } = this.state;
     return (
-      <div className="randomchar">
-        <div className="randomchar__block">
-          <img
-            src={thumbnail}
-            alt="Random character"
-            className="randomchar__img"
-          />
-          <div className="randomchar__info">
-            <p className="randomchar__name">{name}</p>
-            <p className="randomchar__descr">{description}</p>
-            <div className="randomchar__btns">
-              <a href={homepage} className="button button__main">
-                <div className="inner">homepage</div>
-              </a>
-              <a href={wiki} className="button button__secondary">
-                <div className="inner">Wiki</div>
-              </a>
+      <div className="randomchar-box">
+        <div className="randomchar">
+          <div className="randomchar__block">
+            <div className="randomchar__img">
+              <img
+                src={thumbnail}
+                alt="Random character"
+                className="randomchar__img"
+              />
+            </div>
+            <div className="randomchar__info">
+              <p className="randomchar__name">{name}</p>
+              <p
+                className={
+                  description === "There is no description for this character"
+                    ? "randomchar__descr achtung"
+                    : "randomchar__descr"
+                }
+              >
+                {description}
+              </p>
+              <div className="randomchar__btns">
+                <a href={homepage} className="button button__main">
+                  <div className="inner">homepage</div>
+                </a>
+                <a href={wiki} className="button button__secondary">
+                  <div className="inner">Wiki</div>
+                </a>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="randomchar__static">
-          <p className="randomchar__title">
-            Random character for today!
-            <br />
-            Do you want to get to know him better?
-          </p>
-          <p className="randomchar__title">Or choose another one</p>
-          <button className="button button__main">
-            <div className="inner">try it</div>
-          </button>
-          <img src={mjolnir} alt="mjolnir" className="randomchar__decoration" />
+          <div className="randomchar__static">
+            <p className="randomchar__title">
+              Random character for today!
+              <br />
+              Do you want to get to know him better?
+            </p>
+            <p className="randomchar__title">Or choose another one</p>
+            <button className="button button__main">
+              <div className="inner">try it</div>
+            </button>
+            <img
+              src={mjolnir}
+              alt="mjolnir"
+              className="randomchar__decoration"
+            />
+          </div>
         </div>
       </div>
     );
