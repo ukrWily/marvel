6f4a5f41534a1507193eef1b40c2e07b96f43abb
”ukrWily” <ukrwily@gmail.com>
2024-06-26 22:22:12 +0300

    PC propTypes

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -1,4 +1,5 @@
 import { Component } from "react";
+import PropTypes from "prop-types";
 import Spinner from "../spinner/Spinner";
 import ErrorMessage from "../errorMessage/ErrorMessage";
 import Skeleton from "../skeleton/Skeleton";
@@ -120,6 +121,13 @@ const View = ({ char }) => {
       </ul>
     </>
   );
+};
+
+/**
+ * props validation
+ */
+CharInfo.propTypes = {
+  charId: PropTypes.number,
 };
 
 export default CharInfo;
--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,4 +1,5 @@
 import { Component } from "react";
+import PropTypes from "prop-types";
 import Spinner from "../spinner/Spinner";
 import ErrorMessage from "../errorMessage/ErrorMessage";
 import MarvelService from "../../services/MarvelService";
@@ -115,5 +116,12 @@ class CharList extends Component {
     );
   }
 }
+
+/**
+ * props validation
+ */
+CharList.propTypes = {
+  onCharSelected: PropTypes.func.isRequired,
+};
 
 export default CharList;
